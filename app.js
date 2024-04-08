768e089c0c01b3c48b005a84cb014d7d5afe211d
Shivam Pathak <shivampathak5042.com>
2024-04-08 17:50:07 +0530

    smothness added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector(".reset-btn");
-let msgbox=document.querySelector(".message");
+let msgbox = document.querySelector(".message");
 let msg = document.querySelector("#msg");
-let container=document.querySelector(".container");
-let newGame=document.querySelector(".newgame");
-let turn=document.querySelector(".turn");
+let container = document.querySelector(".container");
+let newGame = document.querySelector(".newgame");
+let chance = document.querySelector(".turn");
 
 let trunO = true;
 
@@ -26,40 +26,40 @@ boxes.forEach((box) => {
 
             box.innerText = "O";
             trunO = false;
-            document.body.style.backgroundColor="rgb(248, 118, 118)";
-            turn.innerText="X's turn";
+            document.body.style.backgroundColor = "rgb(248, 118, 118)";
+            chance.innerText = "X's turn";
         } else {
             box.innerText = "X";
             trunO = true;
-            document.body.style.backgroundColor="rgb(79, 176, 245)";
-            turn.innerText="O's turn";
-            
+            document.body.style.backgroundColor = "rgb(79, 176, 245)";
+            chance.innerText = "O's turn";
+
         }
         box.disabled = true;
         checkWinner();
     });
 });
-newGame.addEventListener('click' , ()=>{
+newGame.addEventListener('click', () => {
     trunO = true;
-    for(let box of boxes){
-        box.disabled=false;
-        box.innerText="";
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
     }
-    msgbox.classList.add("hide");
-    document.body.style.backgroundColor="rgb(79, 176, 245)";
-    turn.innerText="O's turn";
+    msgbox.style.display="none";
+    document.body.style.backgroundColor = "rgb(79, 176, 245)";
+    chance.innerText = "O's turn";
 
 });
 
-resetBtn.addEventListener('click' , ()=>{
+resetBtn.addEventListener('click', () => {
     trunO = true;
-    for(let box of boxes){
-        box.disabled=false;
-        box.innerText="";
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
     }
-    msgbox.classList.add("hide");
-    document.body.style.backgroundColor="rgb(79, 176, 245)";
-    turn.innerText="O's turn";
+    msgbox.style.display="none";
+    document.body.style.backgroundColor = "rgb(79, 176, 245)";
+    chance.innerText = "O's turn";
 
 });
 
@@ -68,20 +68,21 @@ const checkWinner = () => {
         // console.log(pattern[0], pattern[1], pattern[2]);
         // // console.log(boxes[pattern[0]], boxes[pattern[1]], boxes[pattern[2]]);
         // console.log(boxes[pattern[0]].innerText , boxes[pattern[1]].innerText, boxes[pattern[2]].innerText);
-       
-        let position1=boxes[pattern[0]].innerText;
-       let position2=boxes[pattern[1]].innerText;
-       let position3=boxes[pattern[2]].innerText;
-       if (position1 != "" &&position2 != "" && position3 != ""){
-        if(position1 === position2 && position2 ===position3){
-            console.log(position1+ " is Winner");
-            msg.innerText= " Congrats player '"+ position1 + "' you won!!";
-            msgbox.classList.remove("hide");
-            for(let box of boxes){
-                box.disabled=true;
-            }
 
+        let position1 = boxes[pattern[0]].innerText;
+        let position2 = boxes[pattern[1]].innerText;
+        let position3 = boxes[pattern[2]].innerText;
+        if (position1 != "" && position2 != "" && position3 != "") {
+            if (position1 === position2 && position2 === position3) {
+                console.log(position1 + " is Winner");
+                msg.innerText = " Congrats player '" + position1 + "' you won!!";
+                msgbox.style.display="flex";
+                chance.innerText = "";
+                for (let box of boxes) {
+                    box.disabled = true;
+                }
+
+            }
         }
-       }
     }
 }
